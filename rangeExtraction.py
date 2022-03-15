21515a99d9494e59728e7d2cb5f9a9ad7bf78fba
mszak10 <49935726+mszak10@users.noreply.github.com>
2022-03-15 19:19:07 +0100

    +rangeExtraction.py

--- rangeExtraction.py
+++ rangeExtraction.py
@@ -1,13 +1,50 @@
-# rangeExtraction.py - TBD
-# TODO not finished: moving to different device
+# rangeExtraction.py - for 3 or more consequitve integers create a range
+# [1, 3, 4, 5, 6, 8] -> [1, 3-6, 8]
 
 def solution(n):
-    dist = {}
-    ret = []
+    retlist = []  # return list
+    j = 0
     for i, val in enumerate(n):
-        print(f'{i}: {abs(n[i]-n[i+1])}')
+        if j != 0:  # PURPOSE: for is skipped j times
+            j -= 1
+            continue
+        start = 0  # PURPOSE: removes a warning (referece before assignment)
+        try:
+            if abs(n[i] - n[i + 1]) == 1:
+                start = val  # PURPOSE: start of range
+                i += 1
+                if abs(n[i] - n[i + 1]) == 1:
+                    i += 1
+                    j += 1
+                    if i + 1 == len(n):  # PURPOSE: removes duplicate element of list
+                        end = n[i]
+                        retlist.append(f'{start}-{end},')
+                        break
+                    while abs(n[i] - n[i + 1]) == 1:
+                        i += 1
+                        j += 1
+                        if i + 1 == len(n):  # PURPOSE: removes duplicate element of list
+                            break
+                    end = n[i]  # PURPOSE: end of range
+                    j += 1
+                    retlist.append(f'{start}-{end},')
+                else:
+                    retlist.append(f'{val},')
+            else:
+                retlist.append(f'{val},')
+        except IndexError:
+            if abs(n[i - 1] - n[i]) == 1 and abs(n[i - 2] - n[i - 1]) == 1:
+                end = i
+                retlist.append(f'{start}-{end},')
+            else:
+                retlist.append(f'{val},')
 
-    return ''.join(ret)
+    retstr = ''  # returned string
+    for r in retlist:  # PURPOSE: list of strings to string
+        retstr += r
+
+    return retstr[:-1]  # PURPOSE [:-1]: removes last comma
 
 
-print(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+print(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+# outputs "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
